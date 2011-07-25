ff95bf901c6bc38cb5043d031395b2fa4ee6a8d9
alAmjadTawfiq Isstaif <isstaif@gmail.com>
2011-07-25 23:11:27 +0300

    SocialWiki pods

---
+++ pods/sw_updater/default_requester.php
@@ -0,0 +1,87 @@
+<?php
+
+	
+include_once("../../lib/Core.php");
+
+// only allow logged in people
+$POD = new PeoplePod(array(
+ 'authSecret'=>$authSecret,
+  'lockdown'=>'login'
+));
+
+	if (defined('WIKI_USERNAME')) 
+	{
+		
+		include_once('wikimate/globals.php');
+
+			//define('WIKI_USERNAME','');
+			//define('WIKI_PASSWORD','');
+			//define('WIKI_API',$wiki->get('api'));
+			//these are predefined by the file updater
+		if (isset($requester)) unset($requester);
+		$requester = new Wikimate;
+		$data = array(
+			'list' => 'recentchanges',
+			'rcprop' => 'user|comment|timestamp|title|ids|sizes|redirect|loginfo|flags',
+			'rcend' => $wiki->get('rcstart'),		
+			'rclimit' => 500
+		);			
+		$results = $requester->query( $data );
+		$changes_count=count($results['query']['recentchanges']);
+
+		if($results && (count($results)>0) && ($changes_count>0) ) 
+		{
+			foreach ($results['query']['recentchanges'] as $recentchange) 
+			{	
+				echo "<pre>";
+				print_r($recentchange);
+				echo "</pre>";
+				echo "<br>";
+				
+				//first we check if the user is registered in our website
+				$username = $recentchange['user'];				
+				$sql = "SELECT value, userId from flags WHERE itemId=".$wiki->get('id')." AND name='mywiki' AND value='".mysql_real_escape_string($username)."';";	
+				$res = mysql_query($sql,$POD->DATABASE);
+				if($res) 
+				{
+					$num = mysql_num_rows($res);
+					if ($num > 0)
+					{
+							//if the user is registered in our website, we are going to publish his activity
+							echo "<p>User is here</p>";
+							//user info
+							$user = mysql_fetch_assoc($res);
+							$userId = $user['userId'];
+							
+							//publishing..
+							$new_activity = $POD->getContent();
+							$new_activity->headline = $recentchange['title'] ;
+							$new_activity->type = 'wikiactivity';
+							$new_activity->userId = $userId;
+							$new_activity->body = $recentchange['comment'];
+							$new_activity->addMeta('rcid',$recentchange['rcid']);
+							$new_activity->addMeta('edit_type',$recentchange['type']);
+							$new_activity->addMeta('wikiId',$wiki->get('id'));
+							
+							//creating date..
+							echo "<br>this: ".$recentchange['timestamp']."<br>";
+							echo "<br>last: ".$wiki->get('rcstart')."<br>";
+							
+							$recentchange['timestamp']= str_replace('T',' ',$recentchange['timestamp']);
+							$recentchange['timestamp'] = str_replace('Z','',$recentchange['timestamp']);							
+							//$new_activity->changeDate = $new_activity->editDate = $recentchange['timestamp'];
+							$new_activity->save();		
+					}
+				}
+			}		
+			$rcts=$results['query']['recentchanges'][0]['timestamp'];
+			$wiki->set('rcstart', inc_time_str($rcts));
+			$wiki->set('last_rcid', $results['query']['recentchanges'][0]['rcid']);	
+			$wiki->save();
+		}		
+		else
+		{
+			echo "no changes";
+		}
+	}	
+?>
---
+++ pods/sw_updater/update.php
@@ -0,0 +1,38 @@
+<?php
+
+include_once("../../lib/Core.php");
+$authSecret=md5('admin@wikitechie.netwikikids');
+// some useful functions :)
+	function inc_time_str($base,$format="Y-m-d H:i:s")
+	{
+		$temp	=strtotime($base);
+		$newtime=strtotime("+1 second",$temp);
+		$newtime=date($format,$newtime);
+		return $newtime;
+	}
+
+// only allow logged in people
+$POD = new PeoplePod(array(
+	'authSecret'=>$_COOKIE['pp_auth'],
+  'lockdown'=>'login'
+));
+
+$POD->header();
+		define('WIKI_USERNAME','');
+		define('WIKI_PASSWORD','');
+
+		
+		$wikis = $POD->getContents(array(
+			'type'=>'wiki'
+		));	
+		
+		
+		foreach($wikis as $wiki){	
+			$_GET["api"] = $wiki->get('api');
+			$_SERVER['REQUEST_METHOD']="GET";
+			include('default_requester.php');
+		}		
+		
+$POD->footer();
+
+?>
