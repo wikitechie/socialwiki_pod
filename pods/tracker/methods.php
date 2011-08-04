1b8307d896844c410caa91a226ac13b0ed54fcaa
Muhammad Al-Syrwan <MhdSyrwan@gmail.com>
2011-08-04 20:55:55 +0300

    set some settings

--- pods/tracker/methods.php
+++ pods/tracker/methods.php
@@ -2,31 +2,31 @@
 
 	
 	// return an array of variables that should be captured in the settings screen
-	function sampleSetup() {
+	function trackerSetup() {
 		return array(
 			'sampleSetting1'=>'Setting 1',
 			'sampleSetting2'=>'Setting 2',
 		);
 	}
 
-	function sampleContentMethod($content) { 
+	function trackerContentMethod($content) { 
 		echo 'This is output from $sampleContentMethod() called on a piece of content with the title "' . $content->headline . '"';
 	}
 
-	function samplePersonMethod($person) { 
-		echo "samplePersonMethod() called on " . $person->nick;	
+	function trackerPersonMethod($person) { 
+		echo "trackerPersonMethod() called on " . $person->nick;	
 	}
 	
 		
-	Content::registerMethod('sampleContentMethod');
-	Person::registerMethod('samplePersonMethod');
+	Content::registerMethod('trackerContentMethod');
+	Person::registerMethod('trackerPersonMethod');
 	
 	Content::addDatabaseFields(array('sampleField'=>array()));
 	
 	function sampleInstall($POD) {
-		$POD->executeSQL('alter table content add sampleField varchar(10)');
+		//$POD->executeSQL('alter table content add sampleField varchar(10)');
 	}
 	
 	function sampleUninstall($POD) {
-		$POD->executeSQL('Alter table content drop sampleField');
+		//$POD->executeSQL('Alter table content drop sampleField');
 	}
