a98c929fe41c2a29e990b5f2c2225440d586daf3
alAmjadTawfiq Isstaif <isstaif@gmail.com>
2011-08-06 13:51:12 +0300

    separated comment area in a separate file

---
+++ themes/socialwiki/content/comment_area.php
@@ -0,0 +1,41 @@
+<?php
+/***********************************************
+* This file is part of PeoplePods
+* (c) xoxco, inc  
+* http://peoplepods.net http://xoxco.com
+*
+* theme/content/short_body.php
+* Defines the body output as included by short.php
+*
+* Documentation for this pod can be found here:
+* http://peoplepods.net/readme/themes
+/**********************************************/
+?>
+		
+				<!-- comments area  -->
+				<div class="comments_area" style="display:none;" >
+				<div id="comments<?= $doc->id; ?>" ></div>
+				<? if ($this->POD->isAuthenticated()) { ?>
+					<div id="comment_form" >
+					<h3 id="reply">Leave a comment</h3>
+					<? $POD->currentUser()->output('avatar'); ?>
+					<div class="attributed_content">
+					<form method="post" id="add_comment" action="#addComment" data-inline="true" data-comments="#comments<?= $doc->id; ?>" data-content="<?= $doc->id; ?>">
+					<textarea border="1px solid" name="comment" class="expanding" id="comment"></textarea>	
+					<input type="submit" value="Post" />
+					</form>
+					</div>
+					<div class="clearer"></div>		
+					</div>
+				<? } else { ?>
+					<div id="comment_form">
+					<a name="reply"></a>
+					<p>
+					<a href="<? $POD->siteRoot(); ?>/join">Register for an account</a> to leave a comment.	
+					If you've already got an account, <a href="<? $POD->siteRoot(); ?>/login">login here</a>.
+					</p>
+					</div>
+				<? } ?>
+				</div>
+
+
--- themes/socialwiki/content/short_body.php
+++ themes/socialwiki/content/short_body.php
@@ -66,31 +66,7 @@
 						<a href="#listComments" data-active="hidden" data-content="<?= $doc->id; ?>" data-comments="#comments<?= $doc->id; ?>" >Show Comments</a>
 					</li>
 				</ul>
-				<!-- comments area  -->
-				<div class="comments_area" style="display:none;" >
-				<div id="comments<?= $doc->id; ?>" ></div>
-				<? if ($this->POD->isAuthenticated()) { ?>
-					<div id="comment_form" >
-					<h3 id="reply">Leave a comment</h3>
-					<? $POD->currentUser()->output('avatar'); ?>
-					<div class="attributed_content">
-					<form method="post" id="add_comment" action="#addComment" data-inline="true" data-comments="#comments<?= $doc->id; ?>" data-content="<?= $doc->id; ?>">
-					<textarea border="1px solid" name="comment" class="expanding" id="comment"></textarea>	
-					<input type="submit" value="Post" />
-					</form>
-					</div>
-					<div class="clearer"></div>		
-					</div>
-				<? } else { ?>
-					<div id="comment_form">
-					<a name="reply"></a>
-					<p>
-					<a href="<? $POD->siteRoot(); ?>/join">Register for an account</a> to leave a comment.	
-					If you've already got an account, <a href="<? $POD->siteRoot(); ?>/login">login here</a>.
-					</p>
-					</div>
-				<? } ?>
-				</div>
+				<? $doc->output('comment_area'); ?>
 
 		</article>
 
